19d9c03194d18e0fad516b3534d532a1aa54cc3c
parksooo <popomarin28@gmail.com>
2024-05-15 17:54:53 +0900

    [feat] : fail HundredMillion_138475
    cause time limits but solve one problem

--- programmers/HundredMillion_138475.java
+++ programmers/HundredMillion_138475.java
@@ -1,31 +1,40 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import javax.lang.model.type.ArrayType;
+
 
 public class HundredMillion_138475 {
     public int[] solution(int e, int[] starts) {
         int[] answer = new int[starts.length];
         int[] count = new int[e + 1];
+        int[][] store = new int[e + 1][2];
         count[0] = -1;
 
         for (int i = 1; i <= e; i++) {
             count[i] = getDivisor(i);
         }
+        store[e][0] = count[e];
+        store[e][1] = e;
+
+        storeDivisor(count, store, e);
 
         for (int i = 0; i < starts.length; i++) {
-            int maxValue = Integer.MIN_VALUE;
-            int maxIndex = 0;
-            for (int j = starts[i]; j <= e; j++) {
-                if (maxValue < count[j]) {
-                    maxIndex = j;
-                    maxValue = count[j];
-                }
-            }
-            answer[i] = maxIndex;
+            answer[i] = store[starts[i]][1];
         }
 
         return answer;
+    }
+
+    private void storeDivisor(int[] divisor, int[][] store, int e) {
+        for (int i = e - 1; i >= 1; i--) {
+            if (divisor[i] >= store[i + 1][0]) {
+                store[i][0] = divisor[i];
+                store[i][1] = i;
+            } else {
+                store[i][0] = store[i + 1][0];
+                store[i][1] = store[i + 1][1];
+            }
+        }
     }
 
     private int getDivisor(int n) {
